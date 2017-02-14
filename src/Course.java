df667dc1ee0df9b5f827291d36937fcf1ececc95
Jian Ping Xu <jxu454@uwo.ca>
2017-02-14 13:55:39 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jian Ping Xu on 2/14/2017.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
